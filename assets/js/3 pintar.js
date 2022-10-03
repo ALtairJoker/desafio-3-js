ed19be7d9584db24a694380e2ca3b5871f4a13c1
Alexis <ajeria@outlook.es>
2022-10-03 18:24:56 -0300

    agregando comentarios

--- assets/js/3 pintar.js
+++ assets/js/3 pintar.js
@@ -5,7 +5,11 @@ pintar()
 
 ele.addEventListener("click", () => pintar('yellow'));
 
+//funcion para resetear color a default
+
 ele.addEventListener("dblclick", () => pintar());
+
+//funcion pintar
 
 function pintar(color ='green'){
     ele.style.backgroundColor = color;
